50c132c1852c50162b52f2595aef61accc3501e6
Kevin233 <205623843+Kevin233B@users.noreply.github.com>
2026-07-02 02:48:19 -0700

    修复路径问题

--- build-mt8797.sh
+++ build-mt8797.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-CLANG_PATH="prebuilts/clang/host/linux-x86/clang-r383902"
+CLANG_PATH="$PWD/prebuilts/clang/host/linux-x86/clang-r383902"
 CLANG_BIN="$CLANG_PATH/bin"
 if [ ! -f "$CLANG_BIN/clang" ]; then
 curl -L -o "$CLANG_BIN/clang" "https://github.com/Kevin233B/TALIH-PD2-Kernel/releases/download/other/clang"
