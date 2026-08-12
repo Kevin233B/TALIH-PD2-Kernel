6283959ad964bdf169d9ae576d025c14a561f730
root <root@DESKTOP-NE7PL5I.localdomain>
2026-08-13 04:17:32 +0800

    fix-v11c: 门控 mtk_crtc_stop 的 STREAM_EOF 等待(8s boot logo 死锁点)

--- drivers/gpu/drm/mediatek/mtk_drm_crtc.c
+++ drivers/gpu/drm/mediatek/mtk_drm_crtc.c
@@ -4537,10 +4537,11 @@ void mtk_crtc_stop(struct mtk_drm_crtc *mtk_crtc, bool need_wait)
 		if (gce_event > 0)
 			cmdq_pkt_wait_no_clear(cmdq_handle, gce_event);
 	} else if (mtk_crtc_is_frame_trigger_mode(&mtk_crtc->base)) {
-		/* 1. wait stream eof & clear tocken */
+		/* 1. wait stream eof & clear tocken (TALIH debug: skip) */
 		/* clear eof token to prevent any config after this command */
-		cmdq_pkt_wfe(cmdq_handle,
-				 mtk_crtc->gce_obj.event[EVENT_STREAM_EOF]);
+		if (0)
+			cmdq_pkt_wfe(cmdq_handle,
+					 mtk_crtc->gce_obj.event[EVENT_STREAM_EOF]);
 
 		/* clear dirty token to prevent trigger loop start */
 		cmdq_pkt_clear_event(
@@ -9437,10 +9438,11 @@ void mtk_crtc_stop_for_pm(struct mtk_drm_crtc *mtk_crtc, bool need_wait)
 		cmdq_pkt_wait_no_clear(cmdq_handle,
 				 mtk_crtc->gce_obj.event[EVENT_WDMA0_EOF]);
 	} else if (mtk_crtc_is_frame_trigger_mode(&mtk_crtc->base)) {
-		/* 1. wait stream eof & clear tocken */
+		/* 1. wait stream eof & clear tocken (TALIH debug: skip) */
 		/* clear eof token to prevent any config after this command */
-		cmdq_pkt_wfe(cmdq_handle,
-				 mtk_crtc->gce_obj.event[EVENT_STREAM_EOF]);
+		if (0)
+			cmdq_pkt_wfe(cmdq_handle,
+					 mtk_crtc->gce_obj.event[EVENT_STREAM_EOF]);
 
 		/* clear dirty token to prevent trigger loop start */
 		cmdq_pkt_clear_event(
